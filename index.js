05f6da6ad10611ced390f0a8370b036e49471adb
Peksford <juhapekkaj.virtanen@gmail.com>
2024-09-26 11:22:14 +0300

    committing blog app

---
+++ index.js
@@ -0,0 +1,17 @@
+const app = require('./app');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`);
+});
+{
+  /*
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+*/
+}
